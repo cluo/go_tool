c97bf1e9551044e0df8329e7c53657fb25a86f5c
hunterhug <gdccmcm14@live.com>
2016-11-08 12:15:39 +0800

    cassandraupdate

--- dbs/cassandra/cassandra.go
+++ dbs/cassandra/cassandra.go
@@ -7,18 +7,21 @@ import (
 )
 
 //配置特别简单，主机名和keyspace
+// cassand config,just need host and keyspace
 type Config struct {
 	Host     []string
 	Keyspace string
 }
 
 //这是一个重要的结构体，cassandra连接包装于此
+// cassandra client
 type Cdb struct {
 	*Config
 	baseSession *gocql.Session
 }
 
 //配置初始化连接
+// init cassandra by config
 func NewCdbWithConf(c *Config) (cdb *Cdb) {
 	cdb = &Cdb{
 		Config: c,
@@ -28,6 +31,7 @@ func NewCdbWithConf(c *Config) (cdb *Cdb) {
 }
 
 //一般初始化连接
+// init cdb just these
 func NewCdb(host []string, keyspace string) (cdb *Cdb) {
 	cdb = &Cdb{
 		Config: &Config{
@@ -40,6 +44,7 @@ func NewCdb(host []string, keyspace string) (cdb *Cdb) {
 }
 
 //连接，出错panic
+// before use must connect,error will be panic,oh!
 func (self *Cdb) Connect() {
 	cluster := gocql.NewCluster(self.Host...)
 	cluster.Keyspace = self.Keyspace
@@ -53,6 +58,7 @@ func (self *Cdb) Connect() {
 }
 
 //别名，为了容易使用
+//alias name easy use
 func New(c *Config) (cdb *Cdb) {
 	cdb = &Cdb{
 		Config: c,
@@ -62,6 +68,7 @@ func New(c *Config) (cdb *Cdb) {
 }
 
 //构造查询语句，包括插入，查找，不仅仅是查询
+//create a cassnara sql,not just for query can be insert
 func (c *Cdb) Query(stmt string, values ...interface{}) *gocql.Query {
 	if c.baseSession == nil {
 		return nil
@@ -70,11 +77,13 @@ func (c *Cdb) Query(stmt string, values ...interface{}) *gocql.Query {
 }
 
 //使用上面的查询语句，开始执行,一般是插入操作
+// Query() then can Exec just for insert
 func (c *Cdb) Exec(q *gocql.Query) error {
 	return q.Exec()
 }
 
 //使用上面的查询语句，开始执行,一般是查找操作
+// Query() then real query,see test
 func (c *Cdb) Iter(q *gocql.Query) *gocql.Iter {
 	return q.Iter()
 }
