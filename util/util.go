1bbcacead8faea0892f293c2344752c25bd7b0ff
hunterhug <gdccmcm14@live.com>
2016-11-30 09:21:54 +0800

    youkonwit!

--- util/util.go
+++ util/util.go
@@ -6,7 +6,15 @@
 package util
 
 import (
+	"crypto/md5"
+	"encoding/base64"
+	"encoding/hex"
 	"errors"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/url"
+	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -74,24 +82,6 @@ func TodayString(level int) string {
 	return time.Now().Format(formats)
 }
 
-//def devidelist(files, num=0):
-//    filestype = type(files)
-//    if not filestype == type([]):
-//        raise Exception("文件切分只能是列表")
-//    length = len(files)
-//    split = {}
-//    if length <= 0:
-//        return split
-//    if num >= length:
-//        raise Exception("文件列表切分过小")
-//    process = length // num
-//    for i in range(num):
-//        split[i] = (files[i * process:(i + 1) * process])
-//    remain = files[num * process:]
-//    for i in range(len(remain)):
-//        split[i % num].append(remain[i])
-//    return split
-
 // change by python
 func DevideStringList(files []string, num int) (map[int][]string, error) {
 	length := len(files)
@@ -106,11 +96,166 @@ func DevideStringList(files []string, num int) (map[int][]string, error) {
 	for i := 0; i < num; i++ {
 		// slice inside has a refer, so must do this append
 		//split[i]=files[i*process : (i+1)*process] wrong!
-		split[i] = append(split[i],files[i*process : (i+1)*process]...)
+		split[i] = append(split[i], files[i*process:(i+1)*process]...)
 	}
 	remain := files[num*process:]
 	for i := 0; i < len(remain); i++ {
 		split[i%num] = append(split[i%num], remain[i])
 	}
 	return split, nil
+}
+
+//字符串base64加密
+func Base64E(urlstring string) string {
+	str := []byte(urlstring)
+	data := base64.StdEncoding.EncodeToString(str)
+	return data
+}
+
+//字符串base64解密
+func Base64D(urlxxstring string) string {
+	data, err := base64.StdEncoding.DecodeString(urlxxstring)
+	if err != nil {
+		return ""
+	}
+	s := fmt.Sprintf("%q", data)
+	s = strings.Replace(s, "\"", "", -1)
+	return s
+}
+
+//url转义
+func UrlE(s string) string {
+	return url.QueryEscape(s)
+}
+
+//url解义
+func UrlD(s string) string {
+	s, e := url.QueryUnescape(s)
+	if e != nil {
+		return e.Error()
+	} else {
+		return s
+	}
+}
+
+//判断文件或文件夹是否存在
+func HasFile(s string) bool {
+	f, err := os.Open(s)
+	if err != nil && os.IsNotExist(err) {
+		return false
+	}
+	f.Close()
+	return true
+}
+
+//File-File复制文件
+func CopyFF(src io.Reader, dst io.Writer) error {
+	_, err := io.Copy(dst, src)
+	return err
+}
+
+//File-String复制文件
+func CopyFS(src io.Reader, dst string) error {
+	f, err := os.OpenFile(dst, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = io.Copy(f, src)
+	return err
+}
+
+//判断是否是文件
+func IsFile(filepath string) bool {
+	fielinfo, err := os.Stat(filepath)
+	if err != nil {
+		return false
+	} else {
+		if fielinfo.IsDir() {
+			return false
+		} else {
+			return true
+		}
+	}
+}
+
+//判断是否是文件夹
+func IsDir(filepath string) bool {
+	fielinfo, err := os.Stat(filepath)
+	if err != nil {
+		return false
+	} else {
+		if fielinfo.IsDir() {
+			return true
+		} else {
+			return false
+		}
+	}
+}
+
+//文件状态
+func FileStatus(filepath string) {
+	fielinfo, err := os.Stat(filepath)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("%v", fielinfo)
+	}
+}
+
+//文件夹下数量
+func SizeofDir(dirPth string) int {
+	if IsDir(dirPth) {
+		files, _ := ioutil.ReadDir(dirPth)
+		return len(files)
+	}
+
+	return 0
+}
+
+//字符串是否在字符串数组中
+func InArray(sa []string, a string) bool {
+	for _, v := range sa {
+		if a == v {
+			return true
+		}
+	}
+	return false
+}
+
+//获取文件后缀
+func GetFileSuffix(f string) string {
+	fa := strings.Split(f, ".")
+	if len(fa) == 0 {
+		return ""
+	} else {
+		return fa[len(fa)-1]
+	}
+}
+
+//create md5 string
+func Strtomd5(s string) string {
+	h := md5.New()
+	h.Write([]byte(s))
+	rs := hex.EncodeToString(h.Sum(nil))
+	return rs
+}
+
+func Md5(str string) string {
+	return Strtomd5(str)
+}
+
+func StringsToJson(str string) string {
+	rs := []rune(str)
+	jsons := ""
+	for _, r := range rs {
+		rint := int(r)
+		if rint < 128 {
+			jsons += string(r)
+		} else {
+			jsons += "\\u" + strconv.FormatInt(int64(rint), 16) // json
+		}
+	}
+
+	return jsons
 }
