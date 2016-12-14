6773652641b2b7379a78c1cd51aa32cf2f62579d
hunterhug <gdccmcm14@live.com>
2016-12-14 16:56:53 +0800

    util

--- util/file.go
+++ util/file.go
@@ -7,6 +7,8 @@ package util
 
 import (
 	"errors"
+	"fmt"
+	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -35,14 +37,14 @@ func ReadfromFile(filepath string) ([]byte, error) {
 }
 
 // Get file info
-func GetFilenameInfo(filepath string)(os.FileInfo, error){
-	fileinfo,err:=os.Stat(filepath)
-	return fileinfo,err
+func GetFilenameInfo(filepath string) (os.FileInfo, error) {
+	fileinfo, err := os.Stat(filepath)
+	return fileinfo, err
 }
 
 // rename
-func Rename(oldfilename string,newfilename string)error{
-	return os.Rename(oldfilename,newfilename)
+func Rename(oldfilename string, newfilename string) error {
+	return os.Rename(oldfilename, newfilename)
 }
 
 //根据传入文件夹名字递归新建文件夹
@@ -107,4 +109,89 @@ func ListDir(dirPth string, suffix string) (files []string, err error) {
 		}
 	}
 	return files, nil
+}
+
+//判断文件或文件夹是否存在
+func HasFile(s string) bool {
+	f, err := os.Open(s)
+	if err != nil && os.IsNotExist(err) {
+		return false
+	}
+	f.Close()
+	return true
+}
+
+//File-File复制文件
+func CopyFF(src io.Reader, dst io.Writer) error {
+	_, err := io.Copy(dst, src)
+	return err
+}
+
+//File-String复制文件
+func CopyFS(src io.Reader, dst string) error {
+	f, err := os.OpenFile(dst, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	_, err = io.Copy(f, src)
+	return err
+}
+
+//判断是否是文件
+func IsFile(filepath string) bool {
+	fielinfo, err := os.Stat(filepath)
+	if err != nil {
+		return false
+	} else {
+		if fielinfo.IsDir() {
+			return false
+		} else {
+			return true
+		}
+	}
+}
+
+//判断是否是文件夹
+func IsDir(filepath string) bool {
+	fielinfo, err := os.Stat(filepath)
+	if err != nil {
+		return false
+	} else {
+		if fielinfo.IsDir() {
+			return true
+		} else {
+			return false
+		}
+	}
+}
+
+//文件状态
+func FileStatus(filepath string) {
+	fielinfo, err := os.Stat(filepath)
+	if err != nil {
+		fmt.Println(err.Error())
+	} else {
+		fmt.Printf("%v", fielinfo)
+	}
+}
+
+//文件夹下数量
+func SizeofDir(dirPth string) int {
+	if IsDir(dirPth) {
+		files, _ := ioutil.ReadDir(dirPth)
+		return len(files)
+	}
+
+	return 0
+}
+
+//获取文件后缀
+func GetFileSuffix(f string) string {
+	fa := strings.Split(f, ".")
+	if len(fa) == 0 {
+		return ""
+	} else {
+		return fa[len(fa)-1]
+	}
 }
--- util/util.go
+++ util/util.go
@@ -6,15 +6,7 @@
 package util
 
 import (
-	"crypto/md5"
-	"encoding/base64"
-	"encoding/hex"
 	"errors"
-	"fmt"
-	"io"
-	"io/ioutil"
-	"net/url"
-	"os"
 	"strconv"
 	"strings"
 	"time"
@@ -105,114 +97,6 @@ func DevideStringList(files []string, num int) (map[int][]string, error) {
 	return split, nil
 }
 
-//字符串base64加密
-func Base64E(urlstring string) string {
-	str := []byte(urlstring)
-	data := base64.StdEncoding.EncodeToString(str)
-	return data
-}
-
-//字符串base64解密
-func Base64D(urlxxstring string) string {
-	data, err := base64.StdEncoding.DecodeString(urlxxstring)
-	if err != nil {
-		return ""
-	}
-	s := fmt.Sprintf("%q", data)
-	s = strings.Replace(s, "\"", "", -1)
-	return s
-}
-
-//url转义
-func UrlE(s string) string {
-	return url.QueryEscape(s)
-}
-
-//url解义
-func UrlD(s string) string {
-	s, e := url.QueryUnescape(s)
-	if e != nil {
-		return e.Error()
-	} else {
-		return s
-	}
-}
-
-//判断文件或文件夹是否存在
-func HasFile(s string) bool {
-	f, err := os.Open(s)
-	if err != nil && os.IsNotExist(err) {
-		return false
-	}
-	f.Close()
-	return true
-}
-
-//File-File复制文件
-func CopyFF(src io.Reader, dst io.Writer) error {
-	_, err := io.Copy(dst, src)
-	return err
-}
-
-//File-String复制文件
-func CopyFS(src io.Reader, dst string) error {
-	f, err := os.OpenFile(dst, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0666)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-	_, err = io.Copy(f, src)
-	return err
-}
-
-//判断是否是文件
-func IsFile(filepath string) bool {
-	fielinfo, err := os.Stat(filepath)
-	if err != nil {
-		return false
-	} else {
-		if fielinfo.IsDir() {
-			return false
-		} else {
-			return true
-		}
-	}
-}
-
-//判断是否是文件夹
-func IsDir(filepath string) bool {
-	fielinfo, err := os.Stat(filepath)
-	if err != nil {
-		return false
-	} else {
-		if fielinfo.IsDir() {
-			return true
-		} else {
-			return false
-		}
-	}
-}
-
-//文件状态
-func FileStatus(filepath string) {
-	fielinfo, err := os.Stat(filepath)
-	if err != nil {
-		fmt.Println(err.Error())
-	} else {
-		fmt.Printf("%v", fielinfo)
-	}
-}
-
-//文件夹下数量
-func SizeofDir(dirPth string) int {
-	if IsDir(dirPth) {
-		files, _ := ioutil.ReadDir(dirPth)
-		return len(files)
-	}
-
-	return 0
-}
-
 //字符串是否在字符串数组中
 func InArray(sa []string, a string) bool {
 	for _, v := range sa {
@@ -221,28 +105,6 @@ func InArray(sa []string, a string) bool {
 		}
 	}
 	return false
-}
-
-//获取文件后缀
-func GetFileSuffix(f string) string {
-	fa := strings.Split(f, ".")
-	if len(fa) == 0 {
-		return ""
-	} else {
-		return fa[len(fa)-1]
-	}
-}
-
-//create md5 string
-func Strtomd5(s string) string {
-	h := md5.New()
-	h.Write([]byte(s))
-	rs := hex.EncodeToString(h.Sum(nil))
-	return rs
-}
-
-func Md5(str string) string {
-	return Strtomd5(str)
 }
 
 func StringsToJson(str string) string {
